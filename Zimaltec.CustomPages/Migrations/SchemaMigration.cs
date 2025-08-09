9d6655a52d243ed4aa468a41818fdb7a50cc5540
mcamara <mcamara@zimaltec.es>
2025-08-09 22:20:35 +0200

    Fixes date format in migration attribute
    Ensures that the date format in the NopMigration attribute is in a valid format.
    The date format is updated to `yyyy-MM-dd HH:mm:ss`.

--- Zimaltec.CustomPages/Migrations/SchemaMigration.cs
+++ Zimaltec.CustomPages/Migrations/SchemaMigration.cs
@@ -4,7 +4,7 @@ using Nop.Data.Migrations;
 using Zimaltec.CustomPages.Domains;
 
 namespace Zimaltec.CustomPages.Migrations;
-[NopMigration("09/08/2025 21:34:19", "Nop.Plugin.Zimaltec.CustomPages schema", MigrationProcessType.Installation)]
+[NopMigration("2025-08-09 21:34:19", "Nop.Plugin.Zimaltec.CustomPages schema", MigrationProcessType.Installation)]
 public class SchemaMigration : AutoReversingMigration
 {
     /// <summary>
